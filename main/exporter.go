5e9cecdda9394d32b5de6ac7018eea89d2a2534a
Ovidijus Balkauskas <570945@gmail.com>
2023-11-28 15:18:02 +0200

    Update 2

--- main/exporter.go
+++ main/exporter.go
@@ -1,56 +1,59 @@
 package main
 
 import (
-    "fmt"
-    "github.com/simonvetter/modbus"
+	"fmt"
+	"os"
+	"systemair-prom-exporter-go/systemairmodbus"
+	"time"
+
+	"github.com/simonvetter/modbus"
 )
 
 func main() {
-    fmt.Println("Starting")
+	fmt.Println("Starting")
+	var err error
 
-    var client  *modbus.ModbusClient
-    var err      error
+	conf := &modbus.ClientConfiguration{
+		URL:      "rtu:///dev/ttyUSB0",
+		Speed:    19200,
+		DataBits: 8,
+		Parity:   modbus.PARITY_NONE,
+		StopBits: 1,
+		Timeout:  500 * time.Millisecond,
+	}
 
-    // for an RTU (serial) device/bus
-    client, err = modbus.NewClient(&modbus.ClientConfiguration{
-        URL:      "rtu:///dev/ttyUSB0",
-        Speed:    19200,                   // default
-        DataBits: 8,                       // default, optional
-        Parity:   modbus.PARITY_NONE,      // default, optional
-        StopBits: 1,                       // default if no parity, optional
-        Timeout:  500 * time.Millisecond,
-    })
+	client := CreateModbusClient(conf)
+	fmt.Println("Client created")
 
-    if err != nil {
-        fmt.Println("Error - modbus client creation failed")
-        // error out if client creation failed
-    }
-    fmt.Println("Client created")
+	// now that the client is created and configured, attempt to connect
+	err = client.Open()
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Modbus client Open() failed with error: %v\n", err)
+		os.Exit(2)
+		// note: multiple Open() attempts can be made on the same client until
+		// the connection succeeds (i.e. err == nil), calling the constructor again
+		// is unnecessary.
+	}
+	fmt.Println("Client opened")
 
-    // now that the client is created and configured, attempt to connect
-    err = client.Open()
-    if err != nil {
-        fmt.Println("Error - modbus client Open() failed")
-        // error out if we failed to connect/open the device
-        // note: multiple Open() attempts can be made on the same client until
-        // the connection succeeds (i.e. err == nil), calling the constructor again
-        // is unnecessary.
-        // likewise, a client can be opened and closed as many times as needed.
-    }
-    fmt.Println("Client opened")
+	var eat_temp float32
 
-    // read a single 16-bit holding register at address 12543
-    var reg16   uint16
-    reg16, err  = client.ReadRegister(12543, modbus.HOLDING_REGISTER)
-    if err != nil {
-        fmt.Println("Error - modbus client reading failed")
-      // error out
-    } else {
-      // use value
-      fmt.Printf("value: %v", reg16)        // as unsigned integer
-      fmt.Printf("value: %v", int16(reg16)) // as signed integer
-    }
+	eat_temp = systemairmodbus.GetTempEAT(client) / 10
+	fmt.Println("EAT temp: ", eat_temp)
 
-    // close the TCP connection/serial port
-    client.Close()
+	// close the TCP connection/serial port
+	client.Close()
+}
+
+func CreateModbusClient(conf *modbus.ClientConfiguration) *modbus.ModbusClient {
+	var client  *modbus.ModbusClient
+	var err      error
+
+	client, err = modbus.NewClient(conf)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Modbus client creation failed with error: %v\n", err)
+		os.Exit(2)
+	}
+
+	return client
 }
