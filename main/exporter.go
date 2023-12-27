52db5fddbb6bf7f48b28d17c53fc6a8423fdd591
Ovidijus Balkauskas <570945@gmail.com>
2023-12-27 17:56:34 +0200

    Add Temperature functions

--- main/exporter.go
+++ main/exporter.go
@@ -38,23 +38,33 @@ func main() {
 
   // Airflow values
 	fmt.Println()
-	fmt.Printf("SAF: %d RPM\n", systemairmodbus.GetFanSAF_RPM(client))
-	fmt.Printf("EAF: %d RPM\n", systemairmodbus.GetFanEAF_RPM(client))
-	fmt.Printf("SAF: %d %%\n", systemairmodbus.GetFanSAFPercentage(client))
-	fmt.Printf("EAF: %d %%\n", systemairmodbus.GetFanEAFPercentage(client))
+	fmt.Printf("SAF: %d RPM\n", systemairmodbus.GetFanSAF_RPM(client)) // hvac_fan_speed_rpm{fan='SAF'}
+	fmt.Printf("EAF: %d RPM\n", systemairmodbus.GetFanEAF_RPM(client))// hvac_fan_speed_rpm{fan='EAF'}
+	fmt.Printf("SAF: %d %%\n", systemairmodbus.GetFanSAFPercentage(client)) // hvac_fan_speed_percentage{fan='SAF'}
+	fmt.Printf("EAF: %d %%\n", systemairmodbus.GetFanEAFPercentage(client)) // hvac_fan_speed_percentage{fan='EAF'}
 
 	// Misc values
 	fmt.Println()
-	fmt.Printf("Humidity: %d %%\n", systemairmodbus.GetHumidity(client))
-	fmt.Printf("Humidity demand: %d %%\n", systemairmodbus.GetHumidityDemand(client))
-	fmt.Printf("IAQ: %s\n", systemairmodbus.GetIAQ(client))
-	fmt.Printf("Usermode: %s\n", systemairmodbus.GetUsermode(client))
+	fmt.Printf("Humidity: %d %%\n", systemairmodbus.GetHumidity(client)) // hvac_humidity_percentage{type='sensor'}
+	fmt.Printf("Humidity demand: %d %%\n", systemairmodbus.GetHumidityDemand(client)) // hvac_humidity_percentage{type='demand'}
+	fmt.Printf("IAQ: %s\n", systemairmodbus.GetIAQ(client)) // hvac_iaq_level{level='<result>'}=1
+	fmt.Printf("Usermode: %s\n", systemairmodbus.GetUsermode(client)) // hvac_usermode_enabled{mode='<result>'}=1
 	fmt.Printf("Usermode remaining: %v\n", systemairmodbus.GetUsermodeRemaining(client))
-	fmt.Printf("Filter remaining: %v\n", systemairmodbus.GetFilterRemaining(client))
+	fmt.Printf("Filter remaining: %v\n", systemairmodbus.GetFilterRemaining(client)) // hvac_airfilter_remaining_seconds
+
+  // Heater values
+	fmt.Println()
 
 	// Temperature values
 	fmt.Println()
-	fmt.Printf("EAT: %.1f C\n", systemairmodbus.GetTempEAT(client))
+	fmt.Printf("Temp supply mode: %s\n", systemairmodbus.GetTempMode(client)) // hvac_temp_mode_enabled
+	fmt.Printf("SATC controller output: %d %%\n", systemairmodbus.GetTempDemandPercentage(client)) // hvac_temp_controller_percentage
+	fmt.Printf("Target room: %.1f C\n", systemairmodbus.GetTempTargetRoom(client)) // hvac_temp_target_degrees{type='room'}
+	fmt.Printf("Target supply: %.1f C\n", systemairmodbus.GetTempTargetSupply(client)) // hvac_temp_target_degrees{type='supply'}
+	fmt.Printf("OAT: %.1f C\n", systemairmodbus.GetTempOAT(client)) // hvac_temp_degrees{sensor='OAT'}
+	fmt.Printf("SAT: %.1f C\n", systemairmodbus.GetTempSAT(client)) // hvac_temp_degrees{sensor='SAT'}
+	fmt.Printf("EAT: %.1f C\n", systemairmodbus.GetTempEAT(client)) // hvac_temp_degrees{sensor='EAT'}
+	fmt.Printf("OHT: %.1f C\n", systemairmodbus.GetTempOHT(client)) // hvac_temp_degrees{sensor='OHT'}
 	
 	// close the TCP connection/serial port
 	client.Close()
--- systemairmodbus/temperature.go
+++ systemairmodbus/temperature.go
@@ -2,9 +2,68 @@ package systemairmodbus
 
 import "github.com/simonvetter/modbus"
 
+// GetTempOAT gets the "Outdoor Air Temperature sensor value (standard)".
+// This is the Outdoor Air Temperature in Celsius.
+// Min -40 C, Max 80 C
+func GetTempOAT(client *modbus.ModbusClient) float32 {
+	return float32(readRegister16(client, 12102, modbus.HOLDING_REGISTER)) / 10
+}
+
+// GetTempSAT gets the "Supply Air Temperature sensor value (standard)".
+// This is the Supply Air Temperature in Celsius.
+// Min -40 C, Max 80 C
+func GetTempSAT(client *modbus.ModbusClient) float32 {
+	return float32(readRegister16(client, 12103, modbus.HOLDING_REGISTER)) / 10
+}
+
 // GetTempEAT gets the "PDM EAT sensor value (standard)".
 // This is the Extract Air Temperature in Celsius.
 // Min -40 C, Max 80 C
 func GetTempEAT(client *modbus.ModbusClient) float32 {
 	return float32(readRegister16(client, 12544, modbus.HOLDING_REGISTER)) / 10
+}
+
+// GetTempOHT gets the "Over Heat Temperature sensor (Electrical Heater)".
+// This is the Over Heat Temperature in Celsius.
+// Min -40 C, Max 80 C
+func GetTempOHT(client *modbus.ModbusClient) float32 {
+	return float32(readRegister16(client, 12108, modbus.HOLDING_REGISTER)) / 10
+}
+
+// GetTempMode gets the "Unit temperature control mode" as a string.
+// The possible modes are:
+// 0 - Supply
+// 1 - Room
+// 2 - Extract
+func GetTempMode(client *modbus.ModbusClient) string {
+	// TODO: use iota
+	switch readRegister16(client, 2031, modbus.HOLDING_REGISTER) {
+	case 0:
+		return "Supply"
+	case 1:
+		return "Room"
+	case 2:
+		return "Extract"
+	}
+	return "Error"
+}
+
+// GetTempDemandPercentage gets the "Output of the SATC" in percentage
+// Min 0 %, Max 100 %.
+func GetTempDemandPercentage(client *modbus.ModbusClient) uint16 {
+	return readRegister16(client, 2055, modbus.INPUT_REGISTER)
+}
+
+// GetTempTargetRoom gets the "Temperature setpoint for the supply air temperature".
+// This is the Target Room Temperature in Celsius.
+// Min 12 C, Max 30 C
+func GetTempTargetRoom(client *modbus.ModbusClient) float32 {
+	return float32(readRegister16(client, 2001, modbus.HOLDING_REGISTER)) / 10
+}
+
+// GetTempTargetSupply gets the "Temperature setpoint for the supply air temperature".
+// This is the Target Supply Temperature in Celsius.
+// Min 12 C, Max 30 C
+func GetTempTargetSupply(client *modbus.ModbusClient) float32 {
+	return float32(readRegister16(client, 2054, modbus.INPUT_REGISTER)) / 10
 }
